7e28dbbb2957957047f58f7d2fe79afa11013697
William Wang <wow7@cornell.edu>
2019-04-08 22:03:47 -0400

    skeleton structure for backend server

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+})
+
+// 404, no matching route found
+app.use((req, res) => {
+    res.status(404).send("Invalid API route");
+})
+
+// route for handling errors
+app.use((err, req, res) => {
+    res.status(400).send(err);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
