9ce0391c1cd9773d215bdc4acd2398656b22fdc0
William Wang <wow7@cornell.edu>
2019-04-12 00:38:00 -0400

    finished assignment

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,38 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data = [];
 
-app.get('/', (req, res) => {
-})
+app.get('/api/contact-cards', (req, res) => {
+    res.send(data);
+});
+
+app.post('/api/add-contact-card', (req, res, next) => {
+    if (!req.body.name || !req.body.email) {
+        next(new Error("Invalid request body"));
+    }
+    else {
+        let valid = true;
+        data.forEach(elt => {
+            if (elt.email === req.body.email) {
+                valid = false;
+            }
+        });
+
+        if (valid) {
+            data.push({ name: req.body.name, email: req.body.email });
+            res.send("OK");
+        }
+        else {
+            res.send("NOT_OK");
+        }
+    }
+});
 
 // 404, no matching route found
 app.use((req, res) => {
     res.status(404).send("Invalid API route");
-})
+});
 
 // route for handling errors
 app.use((err, req, res) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            fetched: false,
+            data: [],
+            nameValue: '',
+            emailValue: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+    }
+
+    handleNameChange = event => {
+        this.setState({ nameValue: event.target.value });
+    }
+
+    handleEmailChange = event => {
+        this.setState({ emailValue: event.target.value });
+    }
+    handleSubmit = async event => {
+        await fetch(`/api/add-contact-card`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: this.state.nameValue, email: this.state.emailValue })
+        });
+        event.preventDefault();
+    }
+
+    componentDidMount = async () => {
+        let response = await fetch('/api/contact-cards');
+        let responseJSON = await response.json();
+        this.setState({ fetched: true, data: responseJSON })
+    }
+
+    render() {
+        const { fetched, data } = this.state;
+
+        return (
+            <div className="App">
+                <h1>Contact Cards</h1>
+                <div className="ContactCardsContainer">
+                    {
+                        fetched ? (data.length !== 0 ? data.map(elt => <div className="ContactCard"><h4>{elt.name}</h4><p>{elt.email}</p></div>) : <p>No contact cards</p>) : <p>Loading...</p>
+                    }
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Name</h3>
+                    <input className="InputBox" type="text" onChange={this.handleNameChange} />
+                    <h3>Email</h3>
+                    <input className="InputBox" type="text" onChange={this.handleEmailChange} />
+                    <button type="submit" className="Button">Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
